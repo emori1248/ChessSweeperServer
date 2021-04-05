ba4ffa647dc00c688f457255c1a4e224911221b5
Carter <rollinc@clarkson.edu>
2021-04-05 19:33:18 -0400

    Replaces old asyncio syntax with new syntax

--- Server/run.py
+++ Server/run.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+import websockets
+import asyncio
+import sys
+from server import client_handler
 
 LISTEN_ADDRESS = ("0.0.0.0", 25568) # 0.0.0.0 represents open to all, don't change. second number is port.
 
-import websockets
-from server import client_handler
 start_server = websockets.serve(client_handler, *LISTEN_ADDRESS)
 
-import asyncio
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_server)
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    print("Closing server")
+    sys.exit()
--- Server/server.py
+++ Server/server.py
@@ -4,28 +4,27 @@ import asyncio
 # messages.
 clients = {} #: {websocket: name}
 
-@asyncio.coroutine
-def client_handler(websocket, path):
+async def client_handler(websocket, path):
     print('New client', websocket)
     print(' ({} existing clients)'.format(len(clients)))
 
 
-    identifier = yield from websocket.recv()
+    identifier = await websocket.recv()
     clients[websocket] = identifier
     for client, _ in clients.items():
-        yield from client.send(identifier)
+        await client.send(identifier)
 
     # Handle messages from this client
     while True:
-        message = yield from websocket.recv()
+        message = await websocket.recv()
         if message is None:
             their_name = clients[websocket]
             del clients[websocket]
             print('Client closed connection', websocket)
             for client, _ in clients.items():
-                yield from client.send(their_name)
+                await client.send(their_name)
             break
 
         # Send message to all clients
         for client, _ in clients.items():
-            yield from client.send('{}: {}'.format(name, message))
+            await client.send('{}: {}'.format(identifier, message))
