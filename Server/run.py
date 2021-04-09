9ce964795e2ed3856460c58fb5ee0169dec8ad7e
Carter Rollins <crollins@lcdrives.com>
2021-04-09 14:13:04 -0400

    Adds Server and Api classes

--- Server/run.py
+++ Server/run.py
@@ -2,16 +2,17 @@
 import websockets
 import asyncio
 import sys
-from server import client_handler
+from server import Server
 
-LISTEN_ADDRESS = ("0.0.0.0", 25568) # 0.0.0.0 represents open to all, don't change. second number is port.
+LISTEN_ADDRESS = ("0.0.0.0", 25568)  # 0.0.0.0 represents open to all, don't change. second number is port.
 
-start_server = websockets.serve(client_handler, *LISTEN_ADDRESS)
+server = Server()
+start_server = websockets.serve(server.client_handler, *LISTEN_ADDRESS)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(start_server)
 try:
-    loop.run_forever()
+	loop.run_forever()
 except KeyboardInterrupt:
-    print("Closing server")
-    sys.exit()
+	print("Closing server")
+	sys.exit()
--- Server/server.py
+++ Server/server.py
@@ -1,30 +1,71 @@
 import asyncio
+import websockets
+import json
+import traceback
 
 # The set of clients connected to this server. It is used to distribute
 # messages.
-clients = {} #: {websocket: name}
-
-async def client_handler(websocket, path):
-    print('New client', websocket)
-    print(' ({} existing clients)'.format(len(clients)))
 
 
-    identifier = await websocket.recv()
-    clients[websocket] = identifier
-    for client, _ in clients.items():
-        await client.send(identifier)
+class Api():
+	def __init__(self, server):
+		self.server = server
 
-    # Handle messages from this client
-    while True:
-        message = await websocket.recv()
-        if message is None:
-            their_name = clients[websocket]
-            del clients[websocket]
-            print('Client closed connection', websocket)
-            for client, _ in clients.items():
-                await client.send(their_name)
-            break
+	async def echoAll(self, args):
+		for client in self.server.clients.keys():
+			await self.server.send(client, args)
 
-        # Send message to all clients
-        for client, _ in clients.items():
-            await client.send('{}: {}'.format(identifier, message))
+
+class Server():
+	def __init__(self):
+		self.clients = {}
+		self.fen = ''
+		self.whitePlayer = None
+		self.blackPlayer = None
+
+	async def client_handler(self, websocket, path):
+		sess = Api(self)
+		print('New client', sess)
+		self.clients[sess] = websocket
+		print(f' ({len(self.clients)} existing clients)')
+
+		# Handle messages from this client
+		try:
+			while True:
+				message = await self.receive(sess)
+
+				if message is None:
+					del self.clients[sess]
+					print('Client closed connection', sess)
+				elif "action" in message:
+					response = {}
+					try:
+						if "args" in message:
+							await Api.__dict__[message["action"]](sess, message["args"])
+						else:
+							await Api.__dict__[message["action"]](sess)
+					except TypeError:
+						traceback.print_exc()
+						response = {
+							"error": "Bad shape of command."
+						}
+					except KeyError:
+						traceback.print_exc()
+						response = {
+							"error": "Invalid command."
+						}
+					if response:
+						await self.send(sess, response)
+				else:
+					# Only supports action format
+					await self.send(sess, {"error": "Invalid packet format"})
+
+		except websockets.exceptions.ConnectionClosedOK:
+			del self.clients[sess]
+			print('Client closed connection', sess)
+
+	async def send(self, sess, message):
+		await self.clients[sess].send(json.dumps(message))
+
+	async def receive(self, sess):
+		return json.loads(await self.clients[sess].recv())
