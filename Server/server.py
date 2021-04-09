49b2674a8abd76b7840ff995464f2cefa919ca54
Carter Rollins <crollins@lcdrives.com>
2021-04-09 17:54:47 -0400

    Works over...

--- Server/server.py
+++ Server/server.py
@@ -6,7 +6,7 @@ import traceback
 
 class GameState():
 	def __init__(self):
-		self.fen = ''
+		self.fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 		self.whitePlayer = None
 		self.blackPlayer = None
 
